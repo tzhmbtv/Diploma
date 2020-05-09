8e2c19941585fb152bb3c8cce036376a2c4dae6f
Elnur <tazhimbetov.e@chocolife.kz>
2020-05-09 18:23:32 +0500

    Рефакторинг

--- resources/views/layouts/sidenav.blade.php
+++ resources/views/layouts/sidenav.blade.php
@@ -3,7 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
 <div class="sidenav">
